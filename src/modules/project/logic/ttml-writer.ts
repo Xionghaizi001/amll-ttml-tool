0fc908b90f8bf62b31a47d7bdf19358269342f1c
Linho <lhp@linho.cc>
2026-04-01 22:23:14 +0800

    chore: remove XSLT

--- src/modules/project/logic/ttml-writer.ts
+++ src/modules/project/logic/ttml-writer.ts
@@ -31,7 +31,6 @@ type LineMetadata = {
 
 export default function exportTTMLText(
 	ttmlLyric: TTMLLyric,
-	pretty = false,
 ): string {
 	const params: LyricLine[][] = [];
 	const lyric = ttmlLyric.lyricLines;
@@ -776,28 +775,5 @@ export default function exportTTMLText(
 	ttRoot.appendChild(body);
 	log("ttml document built", ttRoot);
 
-	if (pretty) {
-		const xsltDoc = new DOMParser().parseFromString(
-			[
-				'<xsl:stylesheet xmlns:xsl="http://www.w3.org/1999/XSL/Transform" version="1.0">',
-				'  <xsl:strip-space elements="*"/>',
-				'  <xsl:template match="para[content-style][not(text())]">',
-				'    <xsl:value-of select="normalize-space(.)"/>',
-				"  </xsl:template>",
-				'  <xsl:template match="node()|@*">',
-				'    <xsl:copy><xsl:apply-templates select="node()|@*"/></xsl:copy>',
-				"  </xsl:template>",
-				'  <xsl:output indent="yes"/>',
-				"</xsl:stylesheet>",
-			].join("\n"),
-			"application/xml",
-		);
-
-		const xsltProcessor = new XSLTProcessor();
-		xsltProcessor.importStylesheet(xsltDoc);
-		const resultDoc = xsltProcessor.transformToDocument(doc);
-
-		return new XMLSerializer().serializeToString(resultDoc);
-	}
 	return new XMLSerializer().serializeToString(doc);
 }
